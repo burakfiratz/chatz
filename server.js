1de38c680f4e3edeaae278aa8c94b0cb3a3ff89f
Burak FIRAT <burakfiratz@gmail.com>
2017-09-30 18:26:09 +0300

    Added rooms list

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express'),
 		http = require('http').Server(app),
 		io = require('socket.io')(http),
 		port = process.env.port || 3000,
-		users = [];
+		users = [],
+		rooms = ['home', 'private', 'game', 'conference'];
 
 http.listen(port, function(){
 	console.log('Listening port:'+ port);
@@ -19,16 +20,28 @@ io.on('connection', function(socket){
 	
 	socket.on('login', function(data){
 		users.push(data);
+		socket.username = data.username;
 		io.sockets.emit('userConnected', data);
 		updateUsersList();
+		updateRoomsList();
 	});
 	
 	function updateUsersList(){
 		io.sockets.emit('usersList', users);
 	};
 	
+	function updateRoomsList(){
+		io.emit('roomsList', rooms);
+	};
+	
 	socket.on('sendMessageToServer', function(data){
 		io.sockets.emit('sendMessageToClient', data);
+	});
+	
+	socket.on('disconnect', function(){
+		io.sockets.emit('userDisconnected', { username : socket.username });
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsersList();
 	});
 	
 });
